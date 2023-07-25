f988380cf6498666e9432af9c70b6cda183b930a
BillyMiles <williammiles201@gmail.com>
2023-07-25 18:02:36 -0400

    navbar update

--- react_frontend/src/components/Navigation.js
+++ react_frontend/src/components/Navigation.js
@@ -5,14 +5,20 @@ import Navbar from 'react-bootstrap/Navbar';
 function Navigation() {
 
     return (
-        <Navbar bg="#004953" data-bs-theme="dark">
+        <Navbar bg="#004953" data-bs-theme="dark" >
             <Container>
                 <Navbar.Brand href="#home">Smartfiler</Navbar.Brand>
-                <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+                <Nav className="justify-content-end">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Container>
+            <Container>
+                <Nav className="justify-content-end">
+                    <Nav.Link href="#login">Login</Nav.Link>
+                    <Nav.Link href="#login">Sign Up</Nav.Link>
+                </Nav>
             </Container>
         </Navbar>
     )
